2a4736b505e8cd9d454143338c230228eb8a4efb
KIRUBHA-SHANKAR-A <727723eucs106@skcet.ac.in>
2025-08-24 22:07:00 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/springapp/configuration/SecurityConfig.java
+++ src/main/java/com/example/springapp/configuration/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://vegan-snack-app.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
